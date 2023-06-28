6c283019c8ec7d007b02513d0f737360b6818799
Fredo St Fleur <fredostfleur23@gmail.com>
2023-06-28 13:26:07 -0400

    - Update store module - Add save data method

--- store.rb
+++ store.rb
@@ -1,31 +1,9 @@
 require 'json'
 module Store
-  # def self.load_books_data
-  #   if File.exists?("books.json")
-  #     File.open("books.json", "r") do |file|
-  #       books = JSON.parse(file.read)
-  #       puts books
-  #     end
-  #   end
-  # end
-
-  # def self.load_people_data
-  #   if File.exists?("people.json")
-  #     File.open("people.json", "r") do |file|
-  #       people = JSON.parse(file.read)
-  #       puts people
-  #     end
-  #   end
-  # end
-
-  # def self.load_rentals_data
-  #   if File.exists?("rentals.json")
-  #     File.open("rentals.json", "r") do |file|
-  #       rentals = JSON.parse(file.read)
-  #       puts rentals
-  #     end
-  #   end
-  # end
+  def self.save(file_path, data)
+    arr = JSON.pretty_generate(data)
+    File.write(file_path, arr.to_s)
+  end
 
   def self.read_data_from_file(file_path)
     data = File.read(file_path)
